a2937198d74edfaa338c881dfdfe8d7d2e899780
j-9j <132179393+j-9j@users.noreply.github.com>
2023-04-30 12:04:47 -0400

    Update WardleyGPT.py

--- WardleyGPT.py
+++ WardleyGPT.py
@@ -7,12 +7,12 @@ st.set_page_config(page_title="Chat with LiveAssist")
 st.title("Chat with LiveAssist")
 # st.sidebar.markdown("Developed by Mark Craddock](https://twitter.com/mcraddock)", unsafe_allow_html=True)
 # st.sidebar.markdown("Current Version: 0.1.4")
-# st.sidebar.markdown("Using GPT-4 API")
+# st.sidebar.markdown("Using GPT-3.5 Turbo API")
 # st.sidebar.markdown("Not optimised")
 st.sidebar.markdown("May run out of OpenAI credits")
 
 # Set OpenAI API model
-model = "gpt-4"
+model = "gpt-3.5-turbo-0301"
 
 def get_initial_message():
     messages=[
