d9695d316887ad8c68b54f33a77a97e0a98c8e85
j-9j <132179393+j-9j@users.noreply.github.com>
2023-04-30 10:38:33 -0400

    Update WardleyGPT.py

--- WardleyGPT.py
+++ WardleyGPT.py
@@ -3,12 +3,12 @@ import streamlit as st
 from streamlit_chat import message
 import openai
 
-st.set_page_config(page_title="Chat with WardleyGPT")
-st.title("Chat with WardleyGPT")
-st.sidebar.markdown("Developed by Mark Craddock](https://twitter.com/mcraddock)", unsafe_allow_html=True)
-st.sidebar.markdown("Current Version: 0.1.4")
-st.sidebar.markdown("Using GPT-4 API")
-st.sidebar.markdown("Not optimised")
+st.set_page_config(page_title="Chat with LiveAssist")
+st.title("Chat with LiveAssist")
+# st.sidebar.markdown("Developed by Mark Craddock](https://twitter.com/mcraddock)", unsafe_allow_html=True)
+# st.sidebar.markdown("Current Version: 0.1.4")
+# st.sidebar.markdown("Using GPT-4 API")
+# st.sidebar.markdown("Not optimised")
 st.sidebar.markdown("May run out of OpenAI credits")
 
 # Set OpenAI API model
